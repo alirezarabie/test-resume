454770c9f9446f4ddc04dbc5f094d14e0383968f
alirezarabie <ali.rabie66@gmail.com>
2024-12-02 15:54:16 +0330

    add change color function

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # how i write it
 
 1. use vite to create vanilla javascript project
-2. use some pre-builded html and css template
-3. add IranSans font and make every thing from ltr to rtl
-4. add persian contents to it
+2. use some html and css template
+3. add IranSans font and make every thing work with rtl direction
+4. add change color function and 5 gentle different colors theme, that change with a button
 
 # to test
 
@@ -11,6 +11,7 @@
 2. run "npm install"
 3. run "npm run dev"
 4. open your browser and go to "http://localhost:5173"
+5. use change color button to see other colors
 
 # to build
 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-import "./style.css";
+setupColorChanger(document.querySelector("#changecolor"));
+
+function setupColorChanger(element) {
+  const colors = ["default", "purple", "green", "red", "orange"];
+  var colorIndex = 0;
+
+  // To load CSS file
+  const loadCSS = (color) => {
+    document
+      .getElementById("style")
+      .setAttribute("href", `/src/style-${color}.css`);
+  };
+
+  // add click event listener for changing color
+  element.addEventListener("click", () => {
+    colorIndex = colorIndex + 1;
+    if (colorIndex === colors.length) colorIndex = 0;
+
+    loadCSS(colors[colorIndex]);
+  });
+}
